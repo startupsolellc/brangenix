f5cb3bfd788c7d584fcdf4787d0e6f2e8379efaa
Muhammet DIKYURT <39614219-mdikyurt@users.noreply.replit.com>
2025-02-13 21:31:11 +0000

    Initial commit

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "drizzle-kit";
+git push -u origin mainimport { defineConfig } from "drizzle-kit";
 
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL, ensure the database is provisioned");
